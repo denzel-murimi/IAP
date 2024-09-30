d260f283fffc2aa230d9aaf8907f5adf4bce7792
denzel-murimi <denzel.kahuthia@strathmore.edu>
2024-09-30 22:14:13 +0300

    Update databaseConnect.php

--- databaseConnect.php
+++ databaseConnect.php
@@ -1,16 +1,21 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "Princedenzey15";
-$dbname = "recipes";
+class Database {
+    private $host = "localhost";
+    private $db_name = "iap_db";
+    private $username = "root";
+    private $password = "Princedenzey15";
+    public $conn;
 
+    public function getConnection() {
+        $this->conn = null;
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=gui", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
-} catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
 }
-?>
