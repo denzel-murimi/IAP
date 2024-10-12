47a6485823691edd00f3655ae7c99e015950f69e
denzel-murimi <denzel.kahuthia@strathmore.edu>
2024-10-12 22:38:57 +0300

    Update 2fa.php

--- 2FA/2fa.php
+++ 2FA/2fa.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Verify 2FA</title>
+    <title>2FA Verification</title>
 </head>
 <body>
     <div class="container mt-5">
